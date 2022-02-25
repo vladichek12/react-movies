714dbc5fcc15db7ed608784e47caa026092a36ac
vlad <misharublov@gmail.com>
2022-02-25 22:43:40 +0300

    project complete

---
+++ src/components/Movies.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Movie } from './Movie';
+
+function Movies(props) {
+    const { movies = [] } = props;
+
+    return (
+        <div className="movies">
+            {movies.length ? (
+                movies.map((movie) => {
+                    return <Movie key={movie.imdbID} {...movie} />;
+                })
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
+    );
+}
+
+export { Movies };
